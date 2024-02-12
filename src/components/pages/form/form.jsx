6adf4a66e3df5349db70e9ba174a694ae00cb313
Ana Gil <anagilrodrig@gmail.com>
2024-02-12 23:19:19 +0100

    Add form.css

--- src/components/pages/form/form.jsx
+++ src/components/pages/form/form.jsx
@@ -1,5 +1,7 @@
 // Fichero src/components/App.jsx
 import { useState } from "react";
+import Layout from "../../../layout";
+import "./form.css";
 
 const Form = () => {
   // Estados del componente
@@ -66,114 +68,116 @@ const Form = () => {
   };
 
   return (
-    <div>
-      <form className="form" onSubmit={handleForm}>
-        <h2>Rellena tus datos para finalizar la compra:</h2>
-        <div className="form">
-          {/* name */}
-          <label className="label-text" htmlFor="name">
-            Escribe un nombre:
-          </label>
-          <input
-            className="input-text"
-            type="text"
-            name="name"
-            id="name"
-            placeholder="María García"
-            value={userName}
-            onChange={handleName}
-          />
-
-          {/* email */}
-          <label className="label-text" htmlFor="email">
-            Escribe un email:
-          </label>
-          <input
-            className="input-text"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="mariagarcia@gmail.com"
-            value={email}
-            onChange={handleEmail}
-          />
-
-          {/* payment type */}
-          <label className="label-text">Indica tu método de pago:</label>
-
-          <div className="input-group-radio">
-            <label className="label-radio" htmlFor="creditCard">
-              Tarjeta de crédito
+    <Layout>
+      <div>
+        <form className="form" onSubmit={handleForm}>
+          <h2>Rellena tus datos para finalizar la compra:</h2>
+          <div className="form">
+            {/* name */}
+            <label className="label-text" htmlFor="name">
+              Escribe un nombre:
             </label>
-            {/* Este radio solo debe aparecer activo cuando paymentType sea creditCard */}
             <input
-              type="radio"
-              name="paymentType"
-              id="creditCard"
-              value="creditCard"
-              checked={paymentType === "creditCard"}
-              onChange={handlePaymentType}
+              className="input-text"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="María García"
+              value={userName}
+              onChange={handleName}
+            />
+
+            {/* email */}
+            <label className="label-text" htmlFor="email">
+              Escribe un email:
+            </label>
+            <input
+              className="input-text"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="mariagarcia@gmail.com"
+              value={email}
+              onChange={handleEmail}
+            />
+
+            {/* payment type */}
+            <label className="label-text">Indica tu método de pago:</label>
+
+            <div className="input-group-radio">
+              <label className="label-radio" htmlFor="creditCard">
+                Tarjeta de crédito
+              </label>
+              {/* Este radio solo debe aparecer activo cuando paymentType sea creditCard */}
+              <input
+                type="radio"
+                name="paymentType"
+                id="creditCard"
+                value="creditCard"
+                checked={paymentType === "creditCard"}
+                onChange={handlePaymentType}
+              />
+            </div>
+
+            <div className="input-group-radio">
+              <label className="label-radio" htmlFor="banktransfer">
+                Transferencia
+              </label>
+              {/* Este radio solo debe aparecer activo cuando paymentType sea cash */}
+              <input
+                type="radio"
+                name="paymentType"
+                id="banktransfer"
+                value="banktransfer"
+                checked={paymentType === "banktransfer"}
+                onChange={handlePaymentType}
+              />
+            </div>
+
+            {/* legal terms */}
+            <label className="label-check" htmlFor="legalTerms">
+              Debes aceptar nuestros términos legales para completar la compra:
+            </label>
+            {/* Este radio solo debe aparecer activo cuando legalTerms sea true */}
+            <input
+              type="checkbox"
+              name="legalTerms"
+              id="legalTerms"
+              checked={legalTerms}
+              onChange={handleLegalTerms}
             />
           </div>
 
-          <div className="input-group-radio">
-            <label className="label-radio" htmlFor="banktransfer">
-              Transferencia
-            </label>
-            {/* Este radio solo debe aparecer activo cuando paymentType sea cash */}
-            <input
-              type="radio"
-              name="paymentType"
-              id="banktransfer"
-              value="banktransfer"
-              checked={paymentType === "banktransfer"}
-              onChange={handlePaymentType}
-            />
+          <div className="preview">
+            <h2>Tus datos son:</h2>
+            <ul>
+              <li>Nombre: {userName}</li>
+              <li>Email: {email}</li>
+
+              <li>Método de pago: {renderPaymentTypeText()}</li>
+              <li>
+                Has aceptado nuestros términos legales:{" "}
+                {legalTerms === true ? "Sí" : "No"}
+              </li>
+            </ul>
           </div>
 
-          {/* legal terms */}
-          <label className="label-check" htmlFor="legalTerms">
-            Debes aceptar nuestros términos legales para completar la compra:
-          </label>
-          {/* Este radio solo debe aparecer activo cuando legalTerms sea true */}
+          {/* reset */}
+          {/* Este botón debe estar inhabilitado mientras el formulario no sea válido */}
           <input
-            type="checkbox"
-            name="legalTerms"
-            id="legalTerms"
-            checked={legalTerms}
-            onChange={handleLegalTerms}
+            className="button"
+            type="submit"
+            value="Enviar"
+            disabled={isValidForm() === false}
           />
-        </div>
 
-        <div className="preview">
-          <h2>Tus datos son:</h2>
-          <ul>
-            <li>Nombre: {userName}</li>
-            <li>Email: {email}</li>
-
-            <li>Método de pago: {renderPaymentTypeText()}</li>
-            <li>
-              Has aceptado nuestros términos legales:{" "}
-              {legalTerms === true ? "Sí" : "No"}
-            </li>
-          </ul>
-        </div>
-
-        {/* reset */}
-        {/* Este botón debe estar inhabilitado mientras el formulario no sea válido */}
-        <input
-          className="button"
-          type="submit"
-          value="Enviar"
-          disabled={isValidForm() === false}
-        />
-
-        {/* send */}
-        <button className="button reset" onClick={handleResetButton}>
-          Limpiar el formulario
-        </button>
-      </form>
-    </div>
+          {/* send */}
+          <button className="button reset" onClick={handleResetButton}>
+            Limpiar el formulario
+          </button>
+        </form>
+      </div>
+    </Layout>
   );
 };
 
